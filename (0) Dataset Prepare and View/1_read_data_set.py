ce10d9e92b86a0214c46033bbe957224c1e00e58
Md. Yasmi Tohabar Evon (2016831021) <silverevon10010@gmail.com>
2021-03-30 23:46:08 +0600

    refactor

--- (0) Dataset Prepare and View/1_read_data_set.py
+++ (0) Dataset Prepare and View/1_read_data_set.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
-data = pd.read_csv("../Datasets/(1) Main Datasets/ben.txt", sep="\t", header=None, names=["English", "Bangla", "Attribution"])
 
-print(data.head())
+if __name__ == '__main__':
+    data = pd.read_csv("../Datasets/(1) Main Datasets/ben.txt", sep="\t", header=None, names=["English", "Bangla", "Attribution"])
+    print(data.head())
